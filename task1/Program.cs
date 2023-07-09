60dd45e8788bd36f556a81d09d2b104a082ff84a
Катерина <katttum@ya.ru>
2023-07-09 14:38:30 +0300

    возвести а в ст б функция

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число A:");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите степень B:");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        int result = RaiseToPower(A, B);
+
+        Console.WriteLine("Результат: " + result);
+    }
+
+    static int RaiseToPower(int baseNumber, int exponent)
+    {
+        int result = 1;
+        for (int i = 1; i <= exponent; i++)
+        {
+            result *= baseNumber;
+        }
+        return result;
+    }
+}
