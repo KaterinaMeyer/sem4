b9817a4b0fa241f3fde48bb13bf35bdbb6ed263c
Катерина <katttum@ya.ru>
2023-07-09 15:12:59 +0300

    сумма цифр с функцией

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числ
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        int sum = DigitSum(number);
+
+        Console.WriteLine("Сумма цифр числа: " + sum);
+    }
+
+    static int DigitSum(int number)
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
